04be78117058d1917e6d7407187369fc38a703db
MIYABETaisei <mytspe46@gmail.com>
2022-05-05 18:49:34 +0900

    💄 外観の調整

--- components/Character.jsx
+++ components/Character.jsx
@@ -1,7 +1,9 @@
 function Character(props) {
   return (
     <div className="w-1/3 my-2">
-      <h1 className="text-center font-bold text-3xl">{props.character.name}</h1>
+      <h1 className="text-white text-center font-bold text-3xl">
+        {props.character.name}
+      </h1>
     </div>
   );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
+        </style>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
