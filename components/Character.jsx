b6efeaa5490c01c298a59ce7936cfdbad0da1f9b
MIYABETaisei <mytspe46@gmail.com>
2022-05-05 17:12:28 +0900

    ✨ Characterコンポーネントの作成

---
+++ components/Character.jsx
@@ -0,0 +1,9 @@
+function Character(props) {
+  return (
+    <div className="w-1/3 my-2">
+      <h1 className="text-center font-bold text-3xl">{props.character.name}</h1>
+    </div>
+  );
+}
+
+export default Character;
